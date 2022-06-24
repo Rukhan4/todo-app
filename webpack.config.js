6f2de92c8cd58accbdae4ba1de6424b785afaf54
Surur Khan <surur.rkhan@gmail.com>
2022-06-23 20:33:50 -0400

    development mode

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'development',
@@ -6,5 +7,6 @@ module.exports = {
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
+        publicPath: "dist/"
     },
 };
