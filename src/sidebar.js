69b0201e66a3a18b30218057ac679d7613c54fea
Surur Khan <surur.rkhan@gmail.com>
2022-07-12 14:25:32 -0400

    Fixed note formatting and prevented repeated openings

--- src/sidebar.js
+++ src/sidebar.js
@@ -5,17 +5,21 @@ var current = document.querySelector(".main");
 
 function openNotes() {
     notes.addEventListener("click", () => {
-        var container = document.createElement("div");
-        container.classList.add("note-container");
-        for (var i = 0; i < 4; i++) {
-            const note = document.createElement("textarea");
-            note.classList.add("note-format");
-            note.placeholder = "New Note: "
-            container.appendChild(note);
+        console.log("here");
+        if (current.children.length == 0) {
+            var container = document.createElement("div");
+            container.classList.add("note-container");
+            for (var i = 0; i < 4; i++) {
+                const note = document.createElement("textarea");
+                note.classList.add("note-format");
+                note.placeholder = "New Note: "
+                container.appendChild(note);
+            }
+            current.appendChild(container);
         }
-        current.appendChild(container);
     })
 }
+
 
 function openHome() {
     home.addEventListener("click", () => {
